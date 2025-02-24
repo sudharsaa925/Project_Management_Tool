a1164fe27747df7eb6f702435fbaa3bef7a57f2c
Sudharsaa <163431575+sudharsaa925@users.noreply.github.com>
2025-02-24 22:23:52 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getAuth, GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDJcqEx46zMVTt6rFth2XUYpJrU02ML7LM",
-  authDomain: "project-management-tool-9e5b6.firebaseapp.com",
-  projectId: "project-management-tool-9e5b6",
-  storageBucket: "project-management-tool-9e5b6.firebasestorage.app",
-  messagingSenderId: "802921559583",
-  appId: "1:802921559583:web:89032417befdb71826c72a",
-  measurementId: "G-FHNZC9C46N"
+  apiKey: "YourKey",
+  authDomain: "YourKey",
+  projectId: "YourKey",
+  storageBucket: "YourKey",
+  messagingSenderId: "YourKey",
+  appId: "YourKey",
+  measurementId: "YourKey"
 };
 
 // Initialize Firebase
